635276125b1e1896f2ddff6e1eebfb03b4cb7d00
정재훈 <70499531+pleasemrlostman@users.noreply.github.com>
2023-11-18 17:48:27 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>안녕하세요</h1>
+      <h1>배포</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
