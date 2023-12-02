7de44850100a6cbdd32282a6953a5137c90fcf23
suniiizz <tptpmm@gmail.com>
2023-12-02 14:13:00 +0900

    🔨 [IMPROVEMENT](custom): test commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
